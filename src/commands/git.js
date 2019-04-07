21ce8853e1e482ccc5c75414122d9597d2ace26d
Tylrin <tylrin@outlook.com>
2019-04-07 13:04:03 +0200

    Git command added.

---
+++ src/commands/git.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const permissions = require("../utilities/commandpermission.json");
+const color = require("../utilities/commandcolor.json");
+
+module.exports.run = async (client, message, arguments) => {
+    // Check permission for the command.
+    if (!message.member.hasPermission(permissions.serverinfo)) return message.reply("You don't have the right to see the server information.");
+
+    // Delete your own command.
+    await message.delete().catch();
+
+    // Create embed.
+    let gitEmbed = new Discord.RichEmbed()
+    .setTitle("The-Source-Bot Git reposetory")
+    .setDescription("This is the reposetory where you can find the code that is used by this bot.")
+    .setURL("https://github.com/Tylrin/The-Source-Bot")
+    .setColor(color.git)
+    .setTimestamp()
+    .setFooter("Git Command", client.user.avatarURL);
+    
+    message.author.send(gitEmbed);
+};
+
+module.exports.config = {
+    name: "git",
+    aliases: [],
+    usage: "<prefix>git",
+    description: ""
+};
